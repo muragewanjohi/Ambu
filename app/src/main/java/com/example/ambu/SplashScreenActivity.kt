25064e69940e47a3405ce2b970100a06c66a53c8
Murage Wanjohi <muragewanjohi@gmail.com>
2022-03-08 12:38:28 +0300

    Auth and Sign In: - Phone - Gmail
    Firebase UI

--- app/src/main/java/com/example/ambu/SplashScreenActivity.kt
+++ app/src/main/java/com/example/ambu/SplashScreenActivity.kt
@@ -1,21 +1,117 @@
 package com.example.ambu
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Completable
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class SplashScreenActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
 
+    companion object{
+        private val LOGIN_REQUEST_CODE = 7171
+    }
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        delaySplashScreen()
+    }
+
+    override fun onStop() {
+        AuthUI.getInstance()
+            .delete(this)
+            .addOnCompleteListener {
+                // ...
+            }
+        // [END auth_fui_delete]
+
+        super.onStop()
+    }
+
+    private fun delaySplashScreen() {
         Completable.timer(3,TimeUnit.SECONDS, AndroidSchedulers.mainThread())
             .subscribe {
                 Toast.makeText(this@SplashScreenActivity, "SplashScreen done !", Toast.LENGTH_SHORT)
                     .show()
             }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        init()
+    }
+
+    private fun init(){
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+
+
+    private fun showLoginLayout() {
+        val authMethodPickerLayout = AuthMethodPickerLayout.Builder(R.layout.layout_sign_in)
+            .setPhoneButtonId(R.id.btn_phone_sign_in)
+            .setGoogleButtonId(R.id.btn_google_sign_in)
+            .build()
+
+        val providers = emptyList<AuthUI.IdpConfig>()
+
+        val signInIntent =
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAuthMethodPickerLayout(authMethodPickerLayout)
+                    .setTheme(R.style.LoginTheme)
+                    .setAvailableProviders(providers)
+                    .setIsSmartLockEnabled(false)
+                    .build()
+
+        signInLauncher.launch(signInIntent)
+
+
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+
+
 }
